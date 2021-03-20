8a579c253ba791c53759dd98986fe1364c7018a7
SphericalKat <amolele@gmail.com>
2021-03-20 12:49:33 +0530

    feat(proxy): read responses of all http requests which return HTML
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"bytes"
+	"github.com/elazarl/goproxy"
+	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/elazarl/goproxy"
+	"strings"
 )
 
-func main()  {
+func main() {
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
+	proxy.Verbose = false
+
+	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+
+	proxy.OnResponse().DoFunc(func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+		header := r.Header.Get("Content-Type")
+		if r != nil && strings.Contains(header, "text/html") {
+			var bodyBytes []byte
+
+			bodyBytes, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				log.Println(err)
+			}
+
+			r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+			log.Println(string(bodyBytes))
+		}
+
+		return r
+	})
 
 	log.Fatal(http.ListenAndServe(":6969", proxy))
 }
